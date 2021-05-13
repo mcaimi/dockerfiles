142f80b6a709eac57eded1d42cb150c094c7bb7e
Marco Caimi <caimi.marco@protonmail.ch>
2021-05-13 14:58:03 +0200

    Updated firefox Dockerfile

--- firefox/config/autoconfig.js
+++ firefox/config/autoconfig.js
@@ -1,3 +1,3 @@
 //
-pref("general.config.obscure_setting", 0)
-pref("general.config.filename", "mozilla.cfg")
+pref("general.config.obscure_setting", 0);
+pref("general.config.filename", "mozilla.cfg");
