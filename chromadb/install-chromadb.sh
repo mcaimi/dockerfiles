3b906272d27a649c9ec1a45a4233e415933b8c3b
Marco Caimi <mcaimi@redhat.com>
2025-05-15 11:47:56 +0200

    Bump chromadb version, add support for OCP

--- chromadb/install-chromadb.sh
+++ chromadb/install-chromadb.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
 USR=${1:-chromadb}
-CHROMAVER=${2:-1.0.7}
+CHROMAVER=${2:-1.0.9}
+KUBEVER=${3:-kubernetes}
 
-python3 -m venv $HOME/.virtualenv/chromadb
-source $HOME/.virtualenv/chromadb/bin/activate
-pip install --upgrade pip
-pip install chromadb==$CHROMAVER
+if [[ $KUBEVER == "openshift" ]]; then
+  echo "INSTALLING FOR OPENSHIFT"
+  VIRTUALENV_DIR="/tmp/.virtualenv/chromadb"
+else
+  echo "INSTALLING FOR KUBERNETES"
+  VIRTUALENV_DIR="$HOME/.virtualenv/chromadb"
+fi
+
+# deploy virtualenv
+python3 -m venv $VIRTUALENV_DIR
+source $VIRTUALENV_DIR/bin/activate
+pip install --no-cache-dir --upgrade pip
+pip install --no-cache-dir chromadb==$CHROMAVER
--- chromadb/run-chromadb.sh
+++ chromadb/run-chromadb.sh
@@ -2,8 +2,18 @@
 
 CHROMAHOST=${1:-localhost}
 CHROMAPORT=${2:-8080}
+KUBEVER=${3:-kubernetes}
 
 # ChromaDB startup script
-source $HOME/.virtualenv/chromadb/bin/activate
+if [[ $KUBEVER == "openshift" ]]; then
+  echo "RUNNING ON OPENSHIFT"
+  VIRTUALENV_DIR="/tmp/.virtualenv/chromadb"
+else
+  echo "RUNNING ON KUBERNETES"
+  VIRTUALENV_DIR="$HOME/.virtualenv/chromadb"
+fi
+
+# run chromadb
+source $VIRTUALENV_DIR/bin/activate
 pip list
 chroma run --host $CHROMAHOST --port $CHROMAPORT --path /chroma_data
